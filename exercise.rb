9b508f267a267217348f591f522dd3e2fc26d32e
Daniel Kafer <Daniel.J.Kafer@gmail.com>
2017-11-25 15:58:04 -0600

    completed exercise

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,20 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    #split string into array and iterate over words
+    return str.split(' ').map!{ |word|
+      if word.length > 4
+        marklar = "marklar"
+        #Check for capital first letter
+        marklar.capitalize! if /\A\p{Lu}/.match(word) 
+        #Check for ending punctuation
+        marklar += $& if /\p{P}\z/.match(word)
+        #replace word with correct form of marklar
+        word = marklar
+      else
+        word
+      end              
+    }.join(" ")
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +26,29 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    fibonacci = []
+    fibonacciEven = 0;
+    nth.times do
+      #push 1 to sequence array for the first two numbers
+      if fibonacci.length == 0 || fibonacci.length == 1
+        fibonacci.push(1)
+      else
+        #Find newest number in array
+        lastIndex = fibonacci.length - 1
+        #Add two newest numbers
+        nextNum = fibonacci[lastIndex] + fibonacci[lastIndex-1]
+        #Push sum to sequence array
+        fibonacci.push(nextNum)
+      end
+    end
+    fibonacci.each{ |num|
+      #Check if num in sequence is even
+      if num % 2 == 0
+        #Add to total if even
+        fibonacciEven += num
+      end
+    }
+    return fibonacciEven
   end
 
 end
